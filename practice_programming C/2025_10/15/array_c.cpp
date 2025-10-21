b55d67d64d57499b80028d497daea028794bcb04
ryongbom <ryongbom@mail.ru>
2025-10-21 21:14:35 +0800

    Create array_c.cpp

---
+++ practice_programming C/2025_10/15/array_c.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string>
+
+int main(void)
+{
+//	char str[7] = "coding"; // [c][o][d][i][n][g][\0]
+//	printf("%s\n", str);
+
+//	char str[] = "coding";
+//	printf("%s\n", str);
+//	printf("%d\n", sizeof(str));
+
+//	char str[] = "coding";
+//	
+//	for (int i = 0; i < sizeof(str); i++)
+//	{
+//		printf("%c\n", str[i]);
+//	}
+
+//	ÊéÊÌ ÂÙ±¡»ô : 1byte
+//	¼¿ºº¶ð ÂÙ±¡»ô : 2byte 
+	
+//	for (int i = 0; i < 128; i++)
+//	{
+//		printf("Ê­º÷¿Ô¿¸´Å ¼³ºã %d : %c\n", i, i);
+//	}
+
+
+	return 0;
+}

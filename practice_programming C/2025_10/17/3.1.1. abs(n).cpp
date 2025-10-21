dd5ab94d36e10e181345ef87b9db0a99ac1a62bb
ryongbom <ryongbom@mail.ru>
2025-10-21 21:20:21 +0800

    Create 3.1.1. abs(n).cpp

---
+++ practice_programming C/2025_10/17/3.1.1. abs(n).cpp
@@ -0,0 +1,19 @@
+// 3.1.1. abs(n).cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main()
+{
+	double n;
+
+	cin >> n;
+
+	cout << abs(n) << endl;
+
+    return 0;
+}
